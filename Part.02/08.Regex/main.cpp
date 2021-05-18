27034c7cd12c0258c9269b537dee8ee0f2029253
AndrewGavluck <gavl.andr96@gmail.com>
2021-05-18 09:06:46 +0300

    add samples for ch 08

---
+++ Part.02/08.Regex/main.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <locale>
+#include <regex> // c++ 11
+#include <string>
+
+#include <boost/regex.hpp>
+
+int main()
+{
+    // sample 1
+    // regex_match
+    {
+        std::cout << "sample 1:\n";
+        // boost
+        {
+            std::string s = "Boost Libraries";
+            boost::regex expr{"\\w+\\s\\w+"};
+            std::cout << std::boolalpha << boost::regex_match(s, expr) << '\n';
+        }
+        // c++11 STL
+        {
+            std::string s = "ST Libraries";
+            std::regex expr{"\\w+\\s\\w+"};
+            std::cmatch m;
+            std::cout << std::boolalpha << std::regex_match(s.c_str(), m, expr) << '\n';
+        }
+    }
+
+    // sample 2
+    // regex_search + smatch
+    {
+        std::cout << "sample 2:\n";
+        // boost
+        {
+            std::string s = "BoostXXLibrarieXXCPP";
+            boost::regex expr{"(\\w+)XX(\\w+)XX(\\w+)"};
+            boost::smatch what;
+            if (boost::regex_search(s, what, expr))
+            {
+                std::cout << what[0] << '\n';
+                std::cout << what[1] << " _ " << what[2] << " _ " << what[3] << '\n';
+            }
+        }
+        // c++11 STL
+        {
+
+            std::string s = "ST Libraries CPP";
+            std::regex expr{"(\\w+)\\s(\\w+)\\s(\\w+)"};
+            std::smatch what;
+            if (std::regex_search(s, what, expr))
+            {
+                std::cout << what[0] << '\n';
+                std::cout << what[1] << " _ " << what[2] << " _ " << what[3] << '\n';
+            }
+        }
+    }
+
+    // sample 3
+    // regex_replace
+    {
+        std::cout << "sample 3:\n";
+        // boost
+        {
+            std::string s = "XXBoostXXLibrariesXX";
+            boost::regex expr{"XX"};
+            std::string fmt{"_*_"};
+            std::cout << boost::regex_replace(s, expr, fmt) << '\n';
+        }
+        // c++11 STL
+        {
+            std::string s = " ST Libraries ";
+            std::regex expr{"\\s"};
+            std::string fmt{"_"};
+            std::cout << std::regex_replace(s, expr, fmt) << '\n';
+        }
+    }
+
+    // sample 4
+    // regex_replace + format string
+    {
+        std::cout << "sample 4:\n";
+        // boost
+        {
+            std::string s = "BoostXXLibraries";
+            boost::regex expr{"(\\w+)\\XX(\\w+)"};
+            std::string fmt{"\\2 \\1"}; // or  fmt{"$2 $1"};
+            std::cout << boost::regex_replace(s, expr, fmt) << '\n';
+        }
+        // c++11 STL
+        {
+            std::string s = "ST Libraries";
+            std::regex expr{"(\\w+)\\s(\\w+)"};
+            std::string fmt{"$2 $1"};
+            std::cout << std::regex_replace(s, expr, fmt) << '\n';
+        }
+    }
+
+    // sample 5
+    // output epected : \2 \1
+    {
+        std::cout << "sample 5:\n";
+        // boost
+        {
+            std::string s = "Boost Libraries";
+            boost::regex expr{"(\\w+)\\s(\\w+)"};
+            std::string fmt{"\\2 \\1"};
+            std::cout << boost::regex_replace(s, expr, fmt,
+                                              boost::regex_constants::format_literal) << '\n';
+        }
+        // c++11 STL
+        {
+            std::string s = "Boost Libraries";
+            boost::regex expr{"(\\w+)\\s(\\w+)"};
+            std::string fmt{"\\2 \\1"};
+            std::cout << boost::regex_replace(s, expr, fmt,
+                                              boost::regex_constants::format_literal) << '\n';
+        }
+    }
+
+    // sample 6
+    //
+    {
+        std::cout << "sample 6:\n";
+        // boost
+        {
+
+        }
+        // c++11 STL
+        {
+        }
+    }
+
+    // sample 7
+    //
+    {
+        std::cout << "sample 7:\n";
+        // boost
+        {
+
+        }
+        // c++11 STL
+        {
+        }
+    }
+
+    // sample 8
+    //
+    {
+        std::cout << "sample 8:\n";
+        // boost
+        {
+
+        }
+        // c++11 STL
+        {
+        }
+    }
+
+    return 0;
+}
